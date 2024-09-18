ae014ad9c4b332051e7e40cc9484376a12e3b5dd
Toico007 <161651633+Toico007@users.noreply.github.com>
2024-09-18 18:22:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,6 +11,23 @@ const perguntas = [ //serve para abrir lista de perguntas
     {
         enunciado: "Pergunta 2",
         alternativas: ["Alternativa 1","Alternativa 2"]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: ["Alternativa 1","Alternativa 2"]
     }
-    
 ]
+
+let posicao = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[posicao];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas(){
+    
+}
+
+mostraPergunta();
